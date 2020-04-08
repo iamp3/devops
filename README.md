f2b7d77ba752ed2c348068b25feb5cdf93eb1716
Prisyazhnyuk Pavel <templar15.2012@gmail.com>
2020-04-08 20:21:25 +0300

    init app

---
+++ README.md
@@ -0,0 +1,4 @@
+# aurora
+Aurora Private AI Assistent
+conda env create -f env.yml 
+run --> python app.py
---
+++ aurora.py
@@ -0,0 +1,118 @@
+import pyttsx3
+import webbrowser
+import smtplib
+import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import wolframalpha
+import os
+import sys
+
+engine = pyttsx3.init('sapi5')
+name = "Pavel"
+client = wolframalpha.Client('QAEXLK-RY9HY2PHAT')
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    print('Computer: ' + audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def greetMe():
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 0 and currentH < 12:
+        speak('Good Morning!')
+
+    if currentH >= 12 and currentH < 18:
+        speak('Good Afternoon!')
+
+    if currentH >= 18 and currentH !=0:
+        speak('Good Evening!')
+
+greetMe()
+
+speak('Hello Pavel, I am your personal assistant!')
+speak('How may I help you?')
+
+
+def myCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language='en-in')
+        print('User: ' + query + '\n')
+
+    except sr.UnknownValueError:
+        speak('Sorry Pavel! I didn\'t get that! Try typing the command!')
+        query = str(input('Command: '))
+
+    return query
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        query = myCommand();
+        query = query.lower()
+
+        if 'open youtube' in query:
+            speak('okay')
+            webbrowser.open('www.youtube.com')
+
+        elif 'open sex' in query:
+            speak('porno time')
+            webbrowser.open('www.pornhub.com')
+
+        elif 'open google' in query:
+            speak('okay')
+            webbrowser.open('www.google.co.in')
+
+        elif 'open gmail' in query:
+            speak('okay')
+            webbrowser.open('www.gmail.com')
+
+        elif "what\'s up" in query or 'how are you' in query:
+            stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy']
+            speak(random.choice(stMsgs))
+
+        elif 'nothing' in query or 'abort' in query or 'stop' in query:
+            speak('okay')
+            speak('Bye Pavel, have a good day.')
+            sys.exit()
+
+        elif 'play music' in query:
+            music_folder = ""
+            music = []
+            random_music = music_folder + random.choice(music) + '.mp3'
+            os.system(random_music)
+
+            speak('Okay, here is your music! Enjoy!')
+
+        else:
+            query = query
+            speak('Searching...')
+            try:
+                try:
+                    res = client.query(query)
+                    results = next(res.results).text
+                    speak('WOLFRAM-ALPHA says - ')
+                    speak('Got it.')
+                    speak(results)
+
+                except:
+                    results = wikipedia.summary(query, sentences=2)
+                    speak('Got it.')
+                    speak('WIKIPEDIA says - ')
+                    speak(results)
+            except:
+                webbrowser.open('www.google.com')
+
+        speak('What next! Pavel!')
